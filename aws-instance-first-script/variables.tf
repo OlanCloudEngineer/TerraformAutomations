71c27b653e8c2aea23a3e9c1a6591f4c3b2d6e83
OlanCloudEngineer <olan.odun@gmail.com>
2021-07-03 22:32:31 +0100

    commit

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -1,9 +1,9 @@
 variable "region" {
-  default = "us-east-2"
+  default = "us-east-1"
 }
 
 variable "ami_id" {       
-
+  
   default = {
     us-east-1    = "ami-035b3c7efe6d061d5"
     eu-west-2    = "ami-132b3c7efe6sdfdsfd"
